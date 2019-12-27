75e99378a7f9184a9e1301b35ba201a7e261b9f4
Bartek Pacia <barpac02@gmail.com>
2019-12-27 10:33:26 +0100

    refactored build mentod in StatsPage

--- lib/screens/home/pages/stats/stats_page.dart
+++ lib/screens/home/pages/stats/stats_page.dart
@@ -4,6 +4,8 @@ import 'package:mentorship_client/remote/models/task.dart';
 import 'package:mentorship_client/remote/user_repository.dart';
 import 'package:mentorship_client/screens/home/pages/stats/bloc/bloc.dart';
 
+/// First page from the left on the HomeScreen. Displays welcome message to the user
+/// and provides some information on latest achievements.
 class StatsPage extends StatefulWidget {
   @override
   _StatsPageState createState() => _StatsPageState();
@@ -19,37 +21,16 @@ class _StatsPageState extends State<StatsPage> {
         if (state is StatsPageSuccess) {
           return ListView(
             children: [
-              Text("Welcome, ${state.homeStats.name}", textScaleFactor: 3),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 24, 0, 24),
+                child: Text("Welcome, ${state.homeStats.name}", textScaleFactor: 2),
+              ),
               Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Pending Requests"),
-                      Text(state.homeStats.pendingRequests.toString()),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Accepted Requests"),
-                      Text(state.homeStats.acceptedRequests.toString()),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Rejected Requests"),
-                      Text(state.homeStats.rejectedRequests.toString()),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Completed Relations"),
-                      Text(state.homeStats.completedRelations.toString()),
-                    ],
-                  ),
+                  _buildRow("Pending Requests", state.homeStats.pendingRequests),
+                  _buildRow("Accepted Requests", state.homeStats.acceptedRequests),
+                  _buildRow("Rejected Requests", state.homeStats.rejectedRequests),
+                  _buildRow("Completed Relations", state.homeStats.completedRelations),
                   for (Task achievement in state.homeStats.achievements)
                     Text(achievement.description),
                 ],
@@ -59,9 +40,23 @@ class _StatsPageState extends State<StatsPage> {
         }
         if (state is StatsPageFailure) {
           return Text(state.message);
+        }
+
+        if (state is StatsPageLoading) {
+          return CircularProgressIndicator();
         } else
           return Text("an error occurred");
       }),
+    );
+  }
+
+  Row _buildRow(String text, int count) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(text),
+        Text(count.toString()),
+      ],
     );
   }
 }
