0dace4453257d6eebd89bc527ab4f184eadafe7e
Bartek Pacia <barpac02@gmail.com>
2020-01-02 16:59:43 +0100

    refactored RelationRepository using generics

--- lib/remote/repositories/relation_repository.dart
+++ lib/remote/repositories/relation_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:chopper/chopper.dart';
 import 'package:logging/logging.dart';
 import 'package:mentorship_client/failure.dart';
 import 'package:mentorship_client/remote/api_manager.dart';
@@ -7,161 +8,85 @@ import 'package:mentorship_client/remote/models/relation.dart';
 import 'package:mentorship_client/remote/requests/relation_requests.dart';
 import 'package:mentorship_client/remote/responses/custom_response.dart';
 
+typedef ApiFunction<T> = Future<Response<T>> Function();
+
 class RelationRepository {
   static final RelationRepository instance = RelationRepository._internal();
 
   RelationRepository._internal();
 
-  // TODO Find a clever way to remove all this boilerplate (e.g error handling)!
-  // Maybe a generic function checking response and catching errors that accepts
-  // service method as argument
+  /// Generic method which handles all possible exceptions that might
+  /// happen when calling API
+  Future<T> callSafely<T>(ApiFunction apiFunction) async {
+    try {
+      final response = await apiFunction();
+
+      if (!response.isSuccessful) {
+        Logger.root.severe("Error: ${response.error}");
+        throw Failure.fromJson(response.error);
+      }
+      return response.body;
+    } on SocketException {
+      throw Failure("No internet connection");
+    } on HttpException {
+      throw Failure("HttpException");
+    } on Exception catch (e) {
+      throw Failure(e.toString());
+    }
+  }
 
   /// Returns all mentorship requests and relations of the current user
   Future<List<Relation>> getAllRelationsAndRequests() async {
-    try {
-      final response = await ApiManager.instance.relationService.getAllRelations();
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.getAllRelations());
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-      List<Relation> relations = [];
-
-      for (dynamic relationJson in response.body) {
-        relations.add(Relation.fromJson(relationJson));
-      }
-
-      return relations;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
+    List<Relation> relations = [];
+    for (var relation in body) {
+      relations.add(Relation.fromJson(relation));
     }
+
+    return relations;
   }
 
   Future<CustomResponse> acceptRelation(int relationId) async {
-    try {
-      final response = await ApiManager.instance.relationService.acceptRelation(relationId);
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.acceptRelation(relationId));
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-      CustomResponse customResponse = CustomResponse.fromJson(response.body);
-
-      return customResponse;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return CustomResponse.fromJson(body);
   }
 
   Future<CustomResponse> rejectRelation(int relationId) async {
-    try {
-      final response = await ApiManager.instance.relationService.rejectRelationship(relationId);
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.rejectRelationship(relationId));
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-
-      CustomResponse customResponse = CustomResponse.fromJson(response.body);
-
-      return customResponse;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return CustomResponse.fromJson(body);
   }
 
   Future<CustomResponse> deleteRelation(int relationId) async {
-    try {
-      final response = await ApiManager.instance.relationService.deleteRelationship(relationId);
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.deleteRelationship(relationId));
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-
-      CustomResponse customResponse = CustomResponse.fromJson(response.body);
-
-      return customResponse;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return CustomResponse.fromJson(body);
   }
 
   Future<CustomResponse> cancelRelation(int relationId) async {
-    try {
-      final response = await ApiManager.instance.relationService.cancelRelationship(relationId);
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.cancelRelationship(relationId));
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-
-      CustomResponse customResponse = CustomResponse.fromJson(response.body);
-
-      return customResponse;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return CustomResponse.fromJson(body);
   }
 
   Future<CustomResponse> sendRequest(RelationRequest relationRequest) async {
-    try {
-      final response = await ApiManager.instance.relationService.sendRequest(relationRequest);
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.sendRequest(relationRequest));
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-
-      CustomResponse customResponse = CustomResponse.fromJson(response.body);
-
-      return customResponse;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return CustomResponse.fromJson(body);
   }
 
   Future<Relation> getCurrentRelation() async {
-    try {
-      final response = await ApiManager.instance.relationService.getCurrentRelation();
+    final body = await callSafely(() async =>
+    await ApiManager.instance.relationService.getCurrentRelation());
 
-      if (!response.isSuccessful) {
-        Logger.root.severe("Error: ${response.error}");
-        throw Failure.fromJson(response.error);
-      }
-
-      Relation currentRelation = Relation.fromJson(response.body);
-      return currentRelation;
-    } on SocketException {
-      throw Failure("No internet connection");
-    } on HttpException {
-      throw Failure("HttpException");
-    } on Exception catch (e) {
-      throw Failure(e.toString());
-    }
+    return Relation.fromJson(body);
   }
 }
