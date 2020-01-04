81b02161d4acb9bb7200ceba2d27e90739e228a9
Bartek Pacia <barpac02@gmail.com>
2020-01-04 02:39:13 +0100

    created dummy RequestsPage

--- lib/screens/home/pages/requests/requests_page.dart
+++ lib/screens/home/pages/requests/requests_page.dart
@@ -8,11 +8,43 @@ class RequestsPage extends StatefulWidget {
 class _RequestsPageState extends State<RequestsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent,
-      child: Center(
-        child: Text("Requests Page!"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: TabBar(
+          labelColor: Theme.of(context).accentColor,
+          tabs: [
+            Tab(text: "Pending".toUpperCase()),
+            Tab(text: "Past".toUpperCase()),
+            Tab(text: "All".toUpperCase()),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            _buildPending(context),
+            _buildPast(context),
+            _buildAll(context),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildPending(BuildContext context) {
+    return Center(
+      child: Text("Pending"),
+    );
+  }
+
+  Widget _buildPast(BuildContext context) {
+    return Center(
+      child: Text("Past"),
+    );
+  }
+
+  Widget _buildAll(BuildContext context) {
+    return Center(
+      child: Text("All"),
     );
   }
 }
