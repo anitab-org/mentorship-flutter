9eb3199faca5aa37de391d5b01955dbca7423fb8
Bartek Pacia <barpac02@gmail.com>
2020-01-10 21:37:59 +0100

    SettingsScreen: added info dialog

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentorship_client/auth/auth_bloc.dart';
 import 'package:mentorship_client/auth/bloc.dart';
+import 'package:package_info/package_info.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -12,7 +13,27 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          _buildItem("About", Icons.info),
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text("Info"),
+            onTap: () async {
+              PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+              showAboutDialog(
+                context: context,
+                applicationIcon: Image.asset(
+                  "assets/images/mentorship_system_logo.png",
+                  width: 50,
+                  height: 50,
+                ),
+                applicationName: "Mentorship App",
+                applicationVersion:
+                    "version ${packageInfo.version}, build ${packageInfo.buildNumber}",
+                applicationLegalese:
+                    "Client app for Mentorship System. Cross-platform version initially created by Bartek Pacia.",
+              );
+            },
+          ),
           _buildItem("Feedback", Icons.feedback),
           ListTile(
             leading: Icon(Icons.exit_to_app),
@@ -23,8 +44,8 @@ class SettingsScreen extends StatelessWidget {
               Navigator.of(context).pop();
             },
           ),
-          _buildItem("Change password", Icons.lock),
-          _buildItem("Delete my account", Icons.delete),
+          _buildItem("Change password", Icons.lock_outline),
+          _buildItem("Delete my account", Icons.delete_outline),
         ],
       ),
     );
