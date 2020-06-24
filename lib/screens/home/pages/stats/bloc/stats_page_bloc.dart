d7ba273452cbe00e86bac5ae5f3d92a28990eca6
Techno-Disaster <nitinnirve@gmail.com>
2020-06-24 11:24:50 +0530

    refresher event

--- lib/screens/home/pages/stats/bloc/stats_page_bloc.dart
+++ lib/screens/home/pages/stats/bloc/stats_page_bloc.dart
@@ -5,6 +5,7 @@ import 'package:logging/logging.dart';
 import 'package:mentorship_client/failure.dart';
 import 'package:mentorship_client/remote/models/home_stats.dart';
 import 'package:mentorship_client/remote/repositories/user_repository.dart';
+import 'package:mentorship_client/screens/home/pages/stats/stats_page.dart';
 
 import './bloc.dart';
 
@@ -19,6 +20,14 @@ class StatsPageBloc extends Bloc<StatsPageEvent, StatsPageState> {
   @override
   Stream<StatsPageState> mapEventToState(StatsPageEvent event) async* {
     if (event is StatsPageShowed) {
+      yield* _mapStatsRequested(event);
+    } else if (event is StatsPageRefresh) {
+      yield* _mapStatsRefreshRequested(event);
+    }
+  }
+
+  Stream<StatsPageState> _mapStatsRequested(StatsPageEvent event) async* {
+    if (event is StatsPageShowed) {
       yield StatsPageLoading();
       try {
         final HomeStats homeStats = await userRepository.getHomeStats();
@@ -26,6 +35,18 @@ class StatsPageBloc extends Bloc<StatsPageEvent, StatsPageState> {
       } on Failure catch (failure) {
         Logger.root.severe(failure.message);
         yield StatsPageFailure(failure.message);
+      }
+    }
+  }
+
+  Stream<StatsPageState> _mapStatsRefreshRequested(StatsPageEvent event) async* {
+    if (event is StatsPageRefresh) {
+      yield StatsPageLoading();
+      try {
+        final HomeStats homeStats = await userRepository.getHomeStats();
+        yield StatsPageSuccess(homeStats);
+      } on Failure catch (_) {
+        yield state;
       }
     }
   }
--- lib/screens/home/pages/stats/stats_page.dart
+++ lib/screens/home/pages/stats/stats_page.dart
@@ -4,6 +4,7 @@ import 'package:mentorship_client/remote/models/task.dart';
 import 'package:mentorship_client/remote/repositories/user_repository.dart';
 import 'package:mentorship_client/screens/home/pages/stats/bloc/bloc.dart';
 import 'package:mentorship_client/widgets/loading_indicator.dart';
+import 'dart:async';
 
 /// First page from the left on the HomeScreen. Displays welcome message to the user
 /// and provides some information on latest achievements.
@@ -15,68 +16,98 @@ class StatsPage extends StatefulWidget {
 }
 
 class _StatsPageState extends State<StatsPage> {
+  Completer<void> _refreshCompleter;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshCompleter = Completer<void>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<StatsPageBloc, StatsPageState>(builder: (context, state) {
-      if (state is StatsPageSuccess) {
-        return Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: Text(
-                  "Welcome, ${state.homeStats.name}!",
-                  textScaleFactor: 2,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+    return BlocConsumer<StatsPageBloc, StatsPageState>(
+      listener: (context, state) {
+        if (state is StatsPageShowed) {
+          StatsPageBloc(userRepository: UserRepository.instance)
+            ..add(
+              StatsPageRefresh(),
+            );
+          _refreshCompleter?.complete();
+          _refreshCompleter = Completer();
+        }
+      },
+      builder: (context, state) {
+        return BlocBuilder<StatsPageBloc, StatsPageState>(builder: (context, state) {
+          if (state is StatsPageSuccess) {
+            return RefreshIndicator(
+              onRefresh: () {
+                BlocProvider.of<StatsPageBloc>(context).add(
+                  StatsPageRefresh(),
+                );
+                return _refreshCompleter.future;
+              },
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Welcome, ${state.homeStats.name}!",
+                        textScaleFactor: 2,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        _buildRow("Pending Requests", state.homeStats.pendingRequests),
+                        _buildRow("Accepted Requests", state.homeStats.acceptedRequests),
+                        _buildRow("Rejected Requests", state.homeStats.rejectedRequests),
+                        _buildRow("Completed Relations", state.homeStats.completedRelations),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 24, 0, 12),
+                          child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Recent Achievements",
+                                style: Theme.of(context).textTheme.headline6,
+                              )),
+                        ),
+                        for (Task achievement in state.homeStats.achievements)
+                          Column(
+                            children: <Widget>[
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    value: true,
+                                    onChanged: (status) => null,
+                                    hoverColor: Theme.of(context).accentColor,
+                                  ),
+                                  Text(achievement.description),
+                                ],
+                              ),
+                              Divider(),
+                            ],
+                          ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-              Column(
-                children: [
-                  _buildRow("Pending Requests", state.homeStats.pendingRequests),
-                  _buildRow("Accepted Requests", state.homeStats.acceptedRequests),
-                  _buildRow("Rejected Requests", state.homeStats.rejectedRequests),
-                  _buildRow("Completed Relations", state.homeStats.completedRelations),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 24, 0, 12),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Recent Achievements",
-                          style: Theme.of(context).textTheme.headline6,
-                        )),
-                  ),
-                  for (Task achievement in state.homeStats.achievements)
-                    Column(
-                      children: <Widget>[
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: true,
-                              onChanged: (status) => null,
-                              hoverColor: Theme.of(context).accentColor,
-                            ),
-                            Text(achievement.description),
-                          ],
-                        ),
-                        Divider(),
-                      ],
-                    ),
-                ],
-              )
-            ],
-          ),
-        );
-      }
-      if (state is StatsPageFailure) {
-        return Text(state.message);
-      }
+            );
+          }
+          if (state is StatsPageFailure) {
+            return Text(state.message);
+          }
 
-      if (state is StatsPageLoading) {
-        return LoadingIndicator();
-      } else
-        return Text("an error occurred");
-    });
+          if (state is StatsPageLoading) {
+            return LoadingIndicator();
+          } else
+            return Text("an error occurred");
+        });
+      },
+    );
   }
 
   Widget _buildRow(String text, int count) {
